96a06154d748bc503367cbd57ef52c8b8fe865ae
Kirill Maltsev <opp.2211@gmail.com>
2023-08-05 23:27:45 +0500

    feat: реализовать сервер статистики

---
+++ ewm-stat-service/stat_server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -0,0 +1,28 @@
+package ru.practicum.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "endpoint_hit")
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String app;
+
+    private String uri;
+
+    private String ip;
+
+    @Column(name = "hit_datetime")
+    private LocalDateTime hitDatetime;
+}
