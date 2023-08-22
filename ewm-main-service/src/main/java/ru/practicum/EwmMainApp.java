6343d27b3e8229b042c78d41f462ca28ab285833
Kirill Maltsev <opp.2211@gmail.com>
2023-08-22 19:46:23 +0500

    feat: exception handler

--- ewm-main-service/src/main/java/ru/practicum/EwmMainApp.java
+++ ewm-main-service/src/main/java/ru/practicum/EwmMainApp.java
@@ -7,8 +7,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import ru.practicum.statsclient.StatsClient;
 
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 public class EwmMainApp {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) {
         SpringApplication.run(EwmMainApp.class, args);
     }
