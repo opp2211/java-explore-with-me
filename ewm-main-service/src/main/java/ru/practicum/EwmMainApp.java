77eb71a5cb0319c1961b4bb0af12b86995f5e495
Kirill Maltsev <opp.2211@gmail.com>
2023-10-13 09:01:15 +0500

    feat: docker-compose

--- ewm-main-service/src/main/java/ru/practicum/EwmMainApp.java
+++ ewm-main-service/src/main/java/ru/practicum/EwmMainApp.java
@@ -1,11 +1,7 @@
 package ru.practicum;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import ru.practicum.statsclient.StatsClient;
 
 import java.time.format.DateTimeFormatter;
 
@@ -15,11 +11,5 @@ public class EwmMainApp {
 
     public static void main(String[] args) {
         SpringApplication.run(EwmMainApp.class, args);
-    }
-
-    @Bean
-    public StatsClient getStatsClient(@Value("${ewm-stat_server.url}") String serverUrl,
-                                      RestTemplateBuilder restTemplateBuilder) {
-        return new StatsClient(serverUrl, restTemplateBuilder);
     }
 }
--- ewm-stat-service/stat_client/src/main/java/ru/practicum/statsclient/StatsClient.java
+++ ewm-stat-service/stat_client/src/main/java/ru/practicum/statsclient/StatsClient.java
@@ -1,10 +1,13 @@
 package ru.practicum.statsclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.dto.EndpointHitDto;
@@ -19,12 +22,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class StatsClient {
 
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private final RestTemplate restTemplate;
 
-    public StatsClient(String serverUrl, RestTemplateBuilder restTemplateBuilder) {
+    @Autowired
+    public StatsClient(@Value("${stats.server.url}") String serverUrl, RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
